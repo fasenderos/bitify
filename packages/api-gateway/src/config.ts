af3a0864435411763d7203b2f87d7b711e5c8f93
Andrea Fassina <fasenderos@gmail.com>
2023-08-24 21:36:11 +0200

    fix: ensure encryption key is 32 chars length

--- packages/api-gateway/src/config.ts
+++ packages/api-gateway/src/config.ts
@@ -21,31 +21,39 @@ export const COOKIES = {
   SESSION: `${name}_session`,
 };
 
-export default (): AppConfig => ({
-  app: {
-    name,
-    version,
-  },
-  auth: {
-    expAccessToken: '15m',
-    expRefreshToken: '7d',
-    expVerifyMail: '8m',
-    expResetPassword: '8m',
-    secretAccessToken: ensureValues('JWT_SECRET_ACCESS_TOKEN'),
-    secretRefreshToken: ensureValues('JWT_SECRET_REFRESH_TOKEN'),
-  },
-  db: {
-    host: ensureValues('POSTGRES_HOST'),
-    port: parseInt(ensureValues('POSTGRES_PORT'), 10),
-    username: ensureValues('POSTGRES_USERNAME'),
-    password: ensureValues('POSTGRES_PASSWORD'),
-    database: ensureValues('POSTGRES_DATABASE'),
-  },
-  encryption: {
-    secret: ensureValues('ENCRYPTION_KEY'),
-  },
-  server: {
-    address: ensureValues('SERVER_ADDRESS'),
-    port: parseInt(ensureValues('SERVER_PORT'), 10),
-  },
-});
+export default (): AppConfig => {
+  const encryptSecret = ensureValues('ENCRYPTION_KEY');
+  if (encryptSecret.length !== 32) {
+    throw new Error('ENCRYPTION_KEY must be a 32 chars length string');
+  }
+  return {
+    app: {
+      name,
+      version,
+    },
+    auth: {
+      exp2FAToken: '2m',
+      expAccessToken: '15m',
+      expRefreshToken: '7d',
+      expVerifyMail: '8m',
+      expResetPassword: '8m',
+      secret2FAToken: ensureValues('JWT_SECRET_2FA_TOKEN'),
+      secretAccessToken: ensureValues('JWT_SECRET_ACCESS_TOKEN'),
+      secretRefreshToken: ensureValues('JWT_SECRET_REFRESH_TOKEN'),
+    },
+    db: {
+      host: ensureValues('POSTGRES_HOST'),
+      port: parseInt(ensureValues('POSTGRES_PORT'), 10),
+      username: ensureValues('POSTGRES_USERNAME'),
+      password: ensureValues('POSTGRES_PASSWORD'),
+      database: ensureValues('POSTGRES_DATABASE'),
+    },
+    encryption: {
+      secret: ensureValues('ENCRYPTION_KEY'),
+    },
+    server: {
+      address: ensureValues('SERVER_ADDRESS'),
+      port: parseInt(ensureValues('SERVER_PORT'), 10),
+    },
+  };
+};
