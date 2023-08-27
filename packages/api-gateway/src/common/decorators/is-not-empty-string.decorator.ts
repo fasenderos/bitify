c5af4849317ee7923e7afcb8bb234196d07a557f
Andrea Fassina <fasenderos@gmail.com>
2023-08-27 16:20:49 +0200

    feat: add decorator for checkin non empty string

---
+++ packages/api-gateway/src/common/decorators/is-not-empty-string.decorator.ts
@@ -0,0 +1,25 @@
+import {
+  ValidationOptions,
+  registerDecorator,
+  isNotEmpty,
+  isString,
+  ValidationArguments,
+} from 'class-validator';
+
+// Check the value is a non empty string
+export function IsNotEmptyString(validationOptions?: ValidationOptions) {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      name: 'isNotEmptyString',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions ?? {},
+      validator: {
+        validate: (value: any): boolean =>
+          isString(value) && isNotEmpty(value.trim()),
+        defaultMessage: (validationArguments?: ValidationArguments): string =>
+          `${validationArguments?.property} should not be an empty string`,
+      },
+    });
+  };
+}
