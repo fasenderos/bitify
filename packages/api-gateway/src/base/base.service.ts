f8c3228c563630c44064705b21292838b731645f
Andrea Fassina <fasenderos@gmail.com>
2023-09-17 18:25:22 +0200

    feat: improve base service and add test

--- packages/api-gateway/src/base/base.service.ts
+++ packages/api-gateway/src/base/base.service.ts
@@ -7,7 +7,10 @@ import {
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { BaseEntity } from './base.entity';
 import { IBaseService } from './interfaces/base-service.interface';
-import { UnprocessableEntityException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 
 export abstract class BaseService<
   Entity extends BaseEntity,
@@ -24,6 +27,10 @@ export abstract class BaseService<
    * @returns The entity
    */
   createEntity(data: CreateDTO, userId: string): Entity {
+    if (!data || !userId)
+      throw new InternalServerErrorException(
+        'Missing data or userId for createEntity method',
+      );
     // Instantiating the entity before saving so hooks run
     return this.repo.create({ ...data, userId: userId });
   }
@@ -34,6 +41,8 @@ export abstract class BaseService<
    * @returns The created resource
    */
   save(data: Entity): Promise<Entity> {
+    if (!data)
+      throw new InternalServerErrorException('Missing data for save method');
     return this.repo.save(data);
   }
 
@@ -57,6 +66,10 @@ export abstract class BaseService<
     filter: FindOptionsWhere<Entity>,
     unselected = false,
   ): Promise<Entity | null> {
+    if (!filter)
+      throw new InternalServerErrorException(
+        'Missing filter for findOne method',
+      );
     if (unselected === true) {
       return this.repo.findOne({
         select: this.getAllTableColumns(),
@@ -73,6 +86,8 @@ export abstract class BaseService<
    * @returns The entity that match the conditions or null.
    */
   findById(id: string, unselected = false): Promise<Entity | null> {
+    if (!id)
+      throw new InternalServerErrorException('Missing id for findById method');
     return this.findOne({ id } as FindOptionsWhere<Entity>, unselected);
   }
 
@@ -86,6 +101,10 @@ export abstract class BaseService<
     filter: FindOptionsWhere<Entity>,
     data: UpdateDTO,
   ): Promise<void> {
+    if (!filter || !data)
+      throw new InternalServerErrorException(
+        'Missing filter or data for update method',
+      );
     // @ts-expect-error Dto should not have userId, but we check anyway at runtime
     if (data.userId)
       throw new UnprocessableEntityException('Ownership can not be changed');
@@ -105,6 +124,10 @@ export abstract class BaseService<
     data: UpdateDTO,
     userId?: string,
   ): Promise<void> {
+    if (!id || !data)
+      throw new InternalServerErrorException(
+        'Missing id or data for updateById method',
+      );
     // @ts-expect-error Dto should not have userId, but we check anyway at runtime
     if (data.userId)
       throw new UnprocessableEntityException('Ownership can not be changed');
@@ -126,6 +149,10 @@ export abstract class BaseService<
    * @param {boolean} soft When true a soft delete is performed otherwise a real delete.
    */
   async delete(filter: FindOptionsWhere<Entity>, soft = true): Promise<void> {
+    if (!filter)
+      throw new InternalServerErrorException(
+        'Missing filter for delete method',
+      );
     await this.repo[soft ? 'softDelete' : 'delete'](filter);
   }
 
@@ -138,6 +165,10 @@ export abstract class BaseService<
    * @param {boolean} soft When true a soft delete is performed otherwise a real delete.
    */
   async deleteById(id: string, userId?: string, soft = true): Promise<void> {
+    if (!id)
+      throw new InternalServerErrorException(
+        'Missing id for deleteById method',
+      );
     await this.repo[soft ? 'softDelete' : 'delete']({
       id,
       ...(userId ? { userId: userId } : {}),
---
+++ packages/api-gateway/test/base/base-service.test.ts
@@ -0,0 +1,403 @@
+import { test } from 'tap';
+import { buildServer, createUser } from '../helper';
+import {
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { RecoveryTokensService } from '../../src/recovery-tokens/recovery-tokens.service';
+import { RecoveryToken } from '../../src/recovery-tokens/entities/recovery-token.entity';
+import { randomUUID } from 'crypto';
+
+// For testing the base service we use the recovery-token service
+// which simply extend the base service without any modification
+// TODO does make sense use a fake entity for that? in case
+test('base service creatEntity() and save() method', async ({
+  equal,
+  teardown,
+}) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  // Test createEntity() method without required params
+  try {
+    // @ts-expect-error we now that userId is required
+    service.createEntity(mockBody);
+  } catch (error) {
+    equal(error instanceof InternalServerErrorException, true);
+  }
+  try {
+    // @ts-expect-error we now that dto is required
+    service.createEntity();
+  } catch (error) {
+    equal(error instanceof InternalServerErrorException, true);
+  }
+  // Test createEntity() method with right params
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  equal(tokenEntity instanceof RecoveryToken, true);
+
+  // Test save() method without required param
+  try {
+    // @ts-expect-error we now that dto is required
+    service.save();
+  } catch (error) {
+    equal(error instanceof InternalServerErrorException, true);
+  }
+
+  // test save() method with right param
+  const token = await service.save(tokenEntity);
+  equal(token instanceof RecoveryToken, true);
+});
+
+test('base service find() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test find() method without params
+  {
+    const res = await service.find();
+    equal(Array.isArray(res), true);
+    res.forEach((token) => {
+      equal(token instanceof RecoveryToken, true);
+    });
+  }
+
+  // Test find() method with not valid filter
+  {
+    const res = await service.find({ where: { id: randomUUID() } });
+    equal(Array.isArray(res), true);
+    equal(res.length, 0);
+  }
+
+  // Test find() method with valid filter
+  {
+    const res = await service.find({ where: { id: token.id } });
+    equal(Array.isArray(res), true);
+    equal(res.length, 1);
+  }
+});
+
+test('base service findOne() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test findOne() method without params
+  {
+    try {
+      // @ts-expect-error we now that a filter is required in findOne
+      await service.findOne();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test findOne() method invalid ID
+  {
+    const res = await service.findOne({ id: randomUUID() });
+    equal(res, null);
+  }
+
+  // Test findOne() method with valid ID
+  {
+    const res = await service.findOne({ id: token.id });
+    equal(res instanceof RecoveryToken, true);
+    equal(res?.id, token.id);
+  }
+});
+
+test('base service findById() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test findById() method without params
+  {
+    try {
+      // @ts-expect-error we now that id is required
+      await service.findById();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test findById() method invalid ID
+  {
+    const res = await service.findById(randomUUID());
+    equal(res, null);
+  }
+
+  // Test findById() method with valid ID
+  {
+    const res = await service.findById(token.id);
+    equal(res instanceof RecoveryToken, true);
+    equal(res?.id, token.id);
+  }
+});
+
+test('base service update() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test update() method without params
+  {
+    try {
+      // @ts-expect-error we now that filter is required
+      await service.update();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+
+    try {
+      // @ts-expect-error we now that dto is required
+      await service.update({ id: token.id });
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test update() method with right params
+  {
+    await service.update({ id: token.id }, { token: 'newtokenUpdate' });
+    const response = await service.findById(token.id);
+    equal(response?.token, 'newtokenUpdate');
+  }
+  {
+    // An update can not pass userId
+    try {
+      await service.update(
+        { id: token.id },
+        { token: 'newtokenWithUseId', userId: user.id },
+      );
+    } catch (error) {
+      equal(error instanceof UnprocessableEntityException, true);
+    }
+  }
+});
+
+test('base service updateById() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+  // Test updateById() method without required params
+  {
+    try {
+      // @ts-expect-error we now that filter is required
+      await service.updateById();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+
+    try {
+      // @ts-expect-error we now that dto is required
+      await service.updateById(token.id);
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test updatedById() method with right params
+  {
+    await service.updateById(token.id, {
+      token: 'newtokenupdateById',
+    });
+    const response = await service.findById(token.id);
+    equal(response?.token, 'newtokenupdateById');
+  }
+  // Test updatedById() method with right params and the userId
+  {
+    await service.updateById(
+      token.id,
+      {
+        token: 'newtokenupdateByIdUserId',
+      },
+      user.id,
+    );
+    const response = await service.findById(token.id);
+    equal(response?.token, 'newtokenupdateByIdUserId');
+  }
+  {
+    // An updatedById can not pass userId
+    try {
+      await service.updateById(token.id, {
+        token: 'newtokenWithUserId',
+        userId: user.id,
+      });
+    } catch (error) {
+      equal(error instanceof UnprocessableEntityException, true);
+    }
+  }
+});
+
+test('base service delete() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test delete() method without params
+  {
+    try {
+      // @ts-expect-error we now that filter is required
+      await service.delete();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test delete() method with right params
+  {
+    await service.delete({ id: token.id });
+    const response = await service.findById(token.id);
+    equal(response, null);
+    {
+      // restore soft deleted token
+      await service.repo.update({ id: token.id }, { deletedAt: null });
+      // token should now exist
+      const response = await service.findById(token.id);
+      equal(response instanceof RecoveryToken, true);
+    }
+  }
+
+  // Test delete() method with soft = false
+  {
+    await service.delete({ id: token.id }, false);
+    const response = await service.findById(token.id);
+    equal(response, null);
+    {
+      // try to restore deleted token
+      await service.repo.update({ id: token.id }, { deletedAt: null });
+      // token should not exist
+      const response = await service.findById(token.id);
+      equal(response, null);
+    }
+  }
+});
+
+test('base service deleteById() method', async ({ equal, teardown }) => {
+  const app = await buildServer();
+  teardown(async () => await app.close());
+
+  const service = app.get(RecoveryTokensService);
+  const { user } = await createUser(app);
+
+  const mockBody = {
+    token: 'somerecoverytoken',
+    expiresAt: new Date(),
+  };
+  const tokenEntity = service.createEntity(mockBody, user.id);
+  const token = await service.save(tokenEntity);
+
+  // Test deleteById() method without required params
+  {
+    try {
+      // @ts-expect-error we now that filter is required
+      await service.deleteById();
+    } catch (error) {
+      equal(error instanceof InternalServerErrorException, true);
+    }
+  }
+
+  // Test deleteById() method without passing userId
+  {
+    await service.deleteById(token.id);
+    const response = await service.findById(token.id);
+    equal(response, null);
+    {
+      // restore soft deleted token
+      await service.repo.update({ id: token.id }, { deletedAt: null });
+      // token should now exist
+      const response = await service.findById(token.id);
+      equal(response instanceof RecoveryToken, true);
+    }
+  }
+
+  // Test deleteById() method with userId
+  {
+    await service.deleteById(token.id, user.id);
+    const response = await service.findById(token.id);
+    equal(response, null);
+    {
+      // restore soft deleted token
+      await service.repo.update({ id: token.id }, { deletedAt: null });
+      // token should now exist
+      const response = await service.findById(token.id);
+      equal(response instanceof RecoveryToken, true);
+    }
+  }
+
+  // Test deleteById() method with soft = false
+  {
+    await service.deleteById(token.id, user.id, false);
+    const response = await service.findById(token.id);
+    equal(response, null);
+    {
+      // try to restore deleted token
+      await service.repo.update({ id: token.id }, { deletedAt: null });
+      // token should not exist
+      const response = await service.findById(token.id);
+      equal(response, null);
+    }
+  }
+});
