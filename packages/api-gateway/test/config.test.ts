3310afb164626eae40536544224b21ca22e5f6cf
Andrea Fassina <fasenderos@gmail.com>
2024-01-31 08:27:30 +0100

    deps: replace turbo with nx

--- packages/api-gateway/test/config.test.ts
+++ packages/api-gateway/test/config.test.ts
@@ -2,6 +2,8 @@ import Config from '../src/config';
 import { name, version } from '../package.json';
 import t from 'tap';
 import { AppConfig } from '../src/common/interfaces';
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + './../.env.template' });
 
 t.test('test config', ({ same, end, equal }) => {
   // Without required params should throw err
@@ -10,11 +12,6 @@ t.test('test config', ({ same, end, equal }) => {
   } catch (err: any) {
     equal(err.message, 'Config missing env.RECAPTCHA_PRIVATE_KEY');
   }
-
-  // Mock required params
-  process.env['RECAPTCHA_PRIVATE_KEY'] = 'somevalue';
-  process.env['EMAIL_FROM'] = 'somevalue';
-  process.env['FRONTEND_BASE_URL'] = 'somevalue';
 
   const config: AppConfig = Config();
   same(config, {
@@ -25,21 +22,24 @@ t.test('test config', ({ same, end, equal }) => {
       expRefreshToken: '7d',
       expVerifyMail: '8m',
       expResetPassword: '8m',
-      recaptchaSecret: 'somevalue',
-      secret2FAToken: 'CHANGE-2FA-TOKEN',
-      secretAccessToken: 'CHANGE-ACCESS-TOKEN',
-      secretRefreshToken: 'CHANGE-REFRESH-TOKEN',
+      recaptchaSecret: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
+      secret2FAToken: 'change-me-jwt-2fa-secret',
+      secretAccessToken: 'change-me-jwt-secret',
+      secretRefreshToken: 'change-me-jwt-refresh-secret',
     },
     db: {
       host: '127.0.0.1',
       port: 5432,
       username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      password: 'mysecretpassword',
+      database: 'my_database',
     },
-    email: { transport: undefined, from: 'somevalue' },
-    encryption: { secret: 'CHANGE-ENCRYPTION-KEY' },
-    frontend: { baseUrl: 'somevalue' },
+    email: {
+      transport: 'smtps://username:password@smtp.example.com',
+      from: 'no-reply <email@somesite.com>',
+    },
+    encryption: { secret: 'change-me-encryption-secret-32-c' },
+    frontend: { baseUrl: 'http://127.0.0.1:3000' },
     server: { address: '127.0.0.1', port: 3001 },
   });
   end();
